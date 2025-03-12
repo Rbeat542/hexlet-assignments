7aa5a6e1f07f848c9479544bcf59cc1cd81c79c2
@hexlet/cli <support@hexlet.io>
2025-03-12 11:50:44 +0300

    submit java-web-ru/dynamic-routes

--- java-web-ru/dynamic-routes/src/main/java/exercise/App.java
+++ java-web-ru/dynamic-routes/src/main/java/exercise/App.java
@@ -18,28 +18,11 @@ public final class App {
             config.bundledPlugins.enableDevLogging();
         });
 
-
-    /*    app.get("/users", ctx -> {
-            //var users = Data.getUsers().subList(0,4);
-            var queryString =ctx.queryString();
-            if (queryString == null) {
-                var users = Data.getUsers().subList(0,5);
-                ctx.json(users);
-            } else {
-                var page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
-                var per = ctx.queryParamAsClass("per", Integer.class).getOrDefault(5);
-                var startIndex = (page - 1) * per;
-                var endIndex = page * per;
-                var usersExact = Data.getUsers().subList(startIndex, endIndex);
-                ctx.json(usersExact);
-            }
-        });
-        return app;*/
-
         app.get("/companies", ctx -> {
             ctx.json(COMPANIES);
         });
-//begin
+
+        //begin
         app.get("/companies/{id}", ctx -> {
             var id = ctx.pathParamAsClass("id", Integer.class).get();
             if (COMPANIES.size() >= id) {
@@ -57,18 +40,9 @@ public final class App {
         app.get("/", ctx -> {
             ctx.result("open something like (you can change id): /companies/5");
         });
-
         return app;
-
     }
 
-    /*public static void show(Context ctx) {
-        var id = ctx.pathParamAsClass("id", Long.class).get();
-        // Позже мы разберем эти конструкции подробнее
-        var user = UserRepository.find(id) // Ищем пользователя в базе по id
-                .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + " not found"));
-    }
-    */
     public static void main(String[] args) {
         Javalin app = getApp();
         app.start(7070);
